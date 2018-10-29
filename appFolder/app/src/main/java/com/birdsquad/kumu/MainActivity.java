b1c8f4a1fb7f6985098968de34a9c6305723c224
camelial <camelial@hawaii.edu>
2018-10-28 21:55:35 -1000

    created login form as main activity

--- appFolder/app/src/main/java/com/birdsquad/kumu/MainActivity.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/MainActivity.java
@@ -1,7 +1,9 @@
 package com.birdsquad.kumu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
+
 }
