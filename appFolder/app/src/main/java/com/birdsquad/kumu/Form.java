ab3b89ac06db4141ec4b451c7d881ddf2ab4ea79
camelial <camelial@hawaii.edu>
2018-10-30 23:39:04 -1000

    starting form and storage constructors and some methods

--- appFolder/app/src/main/java/com/birdsquad/kumu/Form.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/Form.java
@@ -1,7 +1,37 @@
 package com.birdsquad.kumu;
 
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
 public class Form {
 
-    
+    private String speciesName;
+
+    private ArrayList<Bitmap> images;
+
+    /**
+     * Creates a new Form object with just a speciesName (used for SpeciesNameActivity)
+     * Inserts the new form into the Storage object where it will be set as the currentForm
+     * Other activities will update the form with other fields.
+     * @param speciesName the rare plant species name input by the user on SpeciesNameActivity
+     */
+    public Form(String speciesName) {
+        this.speciesName = speciesName;
+        // insert into global Storage variable form list
+    }
+
+    /**
+     * Sets the form's list of images to the images taken by the user in ImagesActivity
+     * @param images
+     */
+    public void addImages(ArrayList<Bitmap> images) {
+        this.images = images;
+    }
+
+    public void addAllOtherFields() {
+        // We don't know what fields yet
+        // Implement this later
+    }
 
 }
--- appFolder/app/src/main/java/com/birdsquad/kumu/Storage.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/Storage.java
@@ -8,4 +8,22 @@ public class Storage {
 
     private Form currentForm;
 
+    /**
+     * Adds a new offline form to offline storage
+     * @param form
+     */
+    public void insertForm(Form form) {
+        forms.add(form);
+        currentForm = form; // The most recent inserted form will always be the current form
+    }
+
+    /**
+     * Syncs all offline forms to the server.
+     */
+    public void syncForms() {
+        // makes a json serializable object of the forms
+        // delete all offline forms here
+        // returns this object
+    }
+
 }
