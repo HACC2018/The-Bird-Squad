a9abb292cced1827bf9f74829f10a21f72a13eb8
camelial <camelial@hawaii.edu>
2018-11-03 00:39:15 -1000

    added all form fields to Form class

--- appFolder/app/src/main/java/com/birdsquad/kumu/Form.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/Form.java
@@ -3,12 +3,78 @@ package com.birdsquad.kumu;
 import android.graphics.Bitmap;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Form {
 
     private String speciesName;
-
+    // Section 1
+    private String wildOrOutplanted;
+    private String taxonName;
     private ArrayList<Bitmap> images;
+    private Date observationDate;
+    private String observerName;
+    private String organizationName;
+    private String island;
+    private String areaCode;
+    private String refSite;
+    private String locationNotes;
+    // needs location gps point
+    // needs elevation
+    // Section 2
+    private boolean reportPopulationStructure;
+    private Number numMaturePlants;
+    private Number numImmaturePlants;
+    private Number numSeedlings;
+    private boolean mostCurrentCensus;
+    // Section 3
+    private boolean reportIndividualPlant;
+    private Number plantNumber;
+    private boolean plantTagged;
+    private String plantGender;
+    private Number plantHeight;
+    private Number plantBaseDiameter;
+    private Number plantAge;
+    private String plantReproductive;
+    private String plantVigor;
+    private Number amountImmatureFruit;
+    private Number amountMatureFruit;
+    private Number amountCuttings;
+    private Number amountAirLayers;
+    private Number amonuntFlowers;
+    // Section 4
+    private boolean reportPopulationData;
+    // Phenology
+    private Number percentVegative;
+    private Number percentBuds;
+    private Number percentFlower;
+    private Number percentImmatureFruit;
+    private Number percentMatureFruit;
+    // Health condition
+    private Number percentHealthy;
+    private Number percentModerate;
+    private Number percentPoor;
+    private Number percentDead;
+    // Light level
+    private Number percentFullSun;
+    private Number percentPartialSun;
+    private Number percentPartialShade;
+    private Number percentDeepShade;
+    // section 5
+    private String[] observatoryClosure;
+    private String[] observatoryHeight;
+    private String[] understoryClosure;
+    private String[] soilDrainage;
+    private String[] topography;
+    private String[] aspect;
+    private String assocObsSpecies;
+    private String assocUndSpecies;
+    private String[] substrate;
+    // section 6
+    private boolean reportThreats;
+    private String threats;
+    private String threatManagementNotes;
+
 
     /**
      * Creates a new Form object with just a speciesName (used for SpeciesNameActivity)
