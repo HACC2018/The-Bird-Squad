1bc2fe2e21bb3770498c34c5b6cef98dcb1f3502
Joshua Nishiguchi <jsn9@hawaii.edu>
2018-11-03 01:18:20 -1000

    Initial controller kinda setup with AJAX to send filter information

--- web/Kumu/app/Http/Controllers/MainController.php
+++ web/Kumu/app/Http/Controllers/MainController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+use App\Form;
+use App\Image;
+use App\IslandMaster;
+use App\Plants;
+use App\User;
+
 class MainController extends Controller
 {
-    //
+	//Filter with post
+	public function RequestReports(Request $request){
+		$it = json_decode($request->json);
+		
+
+		$res = Form::where(function($query) use ($it){
+				$query->where([
+				['IslandID', '=', $it->filter_island],
+				['Age', '=', $it->filter_age]
+			]);
+		})->where(function($query) use ($it){
+				$query->where('PlantTaxaName', '=', $it->filter_plant)->orWhere('PlantTaxaName', '=', $it->filter_plant);
+		})->get();
+		return 'ok';
+	}
 }
--- web/Kumu/routes/web.php
+++ web/Kumu/routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('main');
 });
 
-Route::get('/{filter}', function(){
-	
-});
+Route::post('/', 'MainController@RequestReports');
