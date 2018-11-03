758a594d9d6b0309d81504ab70ea92edf17359bc
camelial <camelial@hawaii.edu>
2018-11-02 23:18:28 -1000

    linked together existing forms with next button

--- appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity.java
@@ -1,7 +1,9 @@
 package com.birdsquad.kumu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+    }
+
+    public void goToSection2(View view) {
+        // check all fields and submit to current form
+        Intent intent = new Intent(this, FormActivity2.class);
+        startActivity(intent);
     }
 }
--- appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity2.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity2.java
@@ -1,7 +1,9 @@
 package com.birdsquad.kumu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FormActivity2 extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class FormActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form2);
+    }
+
+    public void goToSection3(View view) {
+        // check all fields and submit to current form if the user has selected yes
+        Intent intent = new Intent(this, FormActivity3.class);
+        startActivity(intent);
     }
 }
