3fc7ab149c134cd728144f80d51213283b0316ee
camelial <camelial@hawaii.edu>
2018-11-03 01:14:26 -1000

    method to submit section 1 data

--- appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -15,6 +17,32 @@ public class FormActivity extends AppCompatActivity {
 
     public void goToSection2(View view) {
         // check all fields and submit to current form
+        Spinner woSpinner = (Spinner) findViewById(R.id.wildOutplantedDropdown);
+        EditText tNameBox = (EditText) findViewById(R.id.taxonNameBox);
+        EditText obsBox = (EditText) findViewById(R.id.observerNameBox);
+        Spinner islSpinner = (Spinner) findViewById(R.id.islandDropdown);
+        EditText aCodeBox = (EditText) findViewById(R.id.areaCodeBox);
+        EditText rSiteBox = (EditText) findViewById(R.id.refSiteBox);
+        EditText locNoteBox = (EditText) findViewById(R.id.locationNoteBox);
+
+        String wildOutplanted = woSpinner.getSelectedItem().toString();
+        String tName = tNameBox.getText().toString();
+        String obs = obsBox.getText().toString();
+        String island = islSpinner.getSelectedItem().toString();
+        String aCode = aCodeBox.getText().toString();
+        String rSite = rSiteBox.getText().toString();
+        String locNotes = locNoteBox.getText().toString();
+
+        KumuApp.getAppStorage().getCurrentForm().addFieldsSection1(
+                wildOutplanted,
+                tName,
+                obs,
+                island,
+                aCode,
+                rSite,
+                locNotes
+        );
+
         Intent intent = new Intent(this, FormActivity2.class);
         startActivity(intent);
     }
