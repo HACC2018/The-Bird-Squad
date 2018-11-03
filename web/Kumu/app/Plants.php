8c3c041a20262fc3577060ef73f7e276912fd4af
Joshua Nishiguchi <jsn9@hawaii.edu>
2018-11-02 21:18:54 -1000

    Table migration
    Rather than having to go through the database and importing it, just use laravel and do "php artisan migrate"

---
+++ web/Kumu/app/Plants.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlantTbl extends Model
+{
+
+   /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'PlantTbl';
+
+}
+
+?>
