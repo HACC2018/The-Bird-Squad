4353e98b0037d90d98be8ed7104994fdb2f8c712
Bela <mirabela@hawaii.edu>
2018-11-03 23:32:35 -1000

    made form 4 layout

--- appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity4.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity4.java
@@ -3,7 +3,15 @@ package com.birdsquad.kumu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class FormActivity4 extends AppCompatActivity {
 
@@ -11,6 +19,8 @@ public class FormActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form4);
+
+
     }
 
     public void goToSection5(View view) {
