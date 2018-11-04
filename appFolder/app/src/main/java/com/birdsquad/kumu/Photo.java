a880d9fb2cacc0c92b4c158767ea9834482908b8
camelial <camelial@hawaii.edu>
2018-11-03 21:08:42 -1000

    added location stuff, but location is not being retrieved

--- appFolder/app/src/main/java/com/birdsquad/kumu/Photo.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/Photo.java
@@ -1,11 +1,28 @@
 package com.birdsquad.kumu;
 
 import android.graphics.Bitmap;
+import android.location.Location;
 
 public class Photo {
 
     private Bitmap image;
 
-    
+    private Location location;
+
+    public Photo(Bitmap image) {
+        this.image = image;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Bitmap getPhoto() {
+        return this.image;
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
 
 }
