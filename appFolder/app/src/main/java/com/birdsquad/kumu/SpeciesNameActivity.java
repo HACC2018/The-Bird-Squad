ac1dffd6cf51d3a703f2b79d22fcb3e9c5b0dbc5
camelial <camelial@hawaii.edu>
2018-11-04 20:50:22 -1000

    added drawer layout stuff

--- appFolder/app/src/main/java/com/birdsquad/kumu/SpeciesNameActivity.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/SpeciesNameActivity.java
@@ -1,8 +1,11 @@
 package com.birdsquad.kumu;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,10 +14,31 @@ import java.util.ArrayList;
 
 public class SpeciesNameActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_species_name);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
     }
 
     /**
