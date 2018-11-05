929bd66c1f64a5e1f80f566cda6527e9b94e9fe3
Joshua Nishiguchi <jsn9@hawaii.edu>
2018-11-05 06:29:25 -1000

    Added save to login so only see login form once
    Also fixed bug in saveForms in Storage class not saving anything

--- appFolder/app/src/main/java/com/birdsquad/kumu/MainActivity.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/MainActivity.java
@@ -1,9 +1,15 @@
 package com.birdsquad.kumu;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +21,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        Intent intent = new Intent(this, SpeciesNameActivity.class);
-        startActivity(intent);
+        EditText user = (EditText) findViewById(R.id.editText);
+        EditText userpass = (EditText) findViewById(R.id.editText3);
+        String errorString = "";
+        if(user.getText().toString().trim().equals(""))
+            errorString += "Username is required to login";
+        if(userpass.getText().toString().equals("")) {
+            if(!errorString.equals("")){
+                errorString += " || ";
+            }
+            errorString += "Password is required to login";
+        }
+        if(errorString.equals("")){
+            //save login
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(KumuApp.getInstance());
+            SharedPreferences.Editor ed = preferences.edit();
+            ed.putString("login", user.getText().toString() + " " + userpass.getText().toString());
+            ed.commit();
+
+            Intent intent = new Intent(this, SpeciesNameActivity.class);
+            startActivity(intent);
+        } else {
+            //Display error
+            Snackbar errorMessage = Snackbar.make(findViewById(R.id.mainLoginConstraintLayout), errorString, Snackbar.LENGTH_LONG);
+            errorMessage.show();
+        }
     }
 
 }
