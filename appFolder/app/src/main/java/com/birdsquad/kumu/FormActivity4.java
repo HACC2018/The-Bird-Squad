9c1cf40ef1c20fce392fb715fe2f9bbd0d10f10b
Camelia Lai <camelial@hawaii.edu>
2018-11-05 18:06:13 -1000

    completed dummy forms outer fields only

--- appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity4.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity4.java
@@ -20,12 +20,33 @@ public class FormActivity4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form4);
 
+        Switch enterPopulation = (Switch) findViewById(R.id.population_data_switch);
+        final LinearLayout innerFields = (LinearLayout) findViewById(R.id.population_data_layout);
+
+        enterPopulation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    innerFields.setVisibility(View.VISIBLE);
+                }
+                else {
+                    innerFields.setVisibility(View.GONE);
+                }
+            }
+        });
 
     }
 
     public void goToSection5(View view) {
         // check all fields and submit to current form
+        // this method is mostly unimplemented
         Intent intent = new Intent(this, FormActivity5.class);
+        startActivity(intent);
+    }
+
+    public void finishLaterSection4 (View view) {
+        Intent intent = new Intent(this, SpeciesNameActivity.class);
+        Toast.makeText(FormActivity4.this, "Form saved for later.",
+                Toast.LENGTH_SHORT).show();
         startActivity(intent);
     }
 }
--- appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity5.java
+++ appFolder/app/src/main/java/com/birdsquad/kumu/FormActivity5.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class FormActivity5 extends AppCompatActivity {
 
@@ -11,11 +15,32 @@ public class FormActivity5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form5);
+
+        Switch enterHabitat = (Switch) findViewById(R.id.habitat_characteristics_switch);
+        final LinearLayout innerFields = (LinearLayout) findViewById(R.id.habitat_characteristics_layout);
+
+        enterHabitat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    innerFields.setVisibility(View.VISIBLE);
+                }
+                else {
+                    innerFields.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     public void goToSection6(View view) {
         // check all fields and submit to current form if user selected yes
         Intent intent = new Intent(this, FormActivity6.class);
+        startActivity(intent);
+    }
+
+    public void finishLaterSection5(View view) {
+        Intent intent = new Intent(this, SpeciesNameActivity.class);
+        Toast.makeText(FormActivity5.this, "Form saved for later.",
+                Toast.LENGTH_SHORT).show();
         startActivity(intent);
     }
 }
